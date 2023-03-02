8b2dc5b110f49501c3f83bbe2d9824db1bbd6a74
Ruslan Palagin <ruslan.palagin.ck@gmail.com>
2023-03-02 16:28:21 +0200

    #master: rm log

--- src/defaultValidators/isNumeric.js
+++ src/defaultValidators/isNumeric.js
@@ -1,5 +1,4 @@
 module.exports = ({ value, t }) => {
-    console.log('typeof value', typeof value)
     if (typeof value === 'number' && !Number.isNaN(value)) return;
     if (Number.isNaN(value) || parseFloat(value).toString() !== value) {
         return t('isNumeric')
